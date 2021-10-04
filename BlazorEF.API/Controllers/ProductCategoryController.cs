78835c93a873239a568a5fbad862fdbe8d3c8030
dattrantienht <74689952+dattrantienht@users.noreply.github.com>
2021-10-04 08:31:35 +0700

    add Web Api project

---
+++ BlazorEF.API/Controllers/ProductCategoryController.cs
@@ -0,0 +1,29 @@
+﻿using BlazorEF.Application.Interfaces;
+using BlazorEF.Application.ViewModels.Product;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorEF.API.Controllers
+{
+    [Route("api/productcategory")]
+    [ApiController]
+    public class ProductCategoryController : ControllerBase
+    {
+        private readonly IProductCategoryService _productCategoryService;
+
+        public ProductCategoryController(IProductCategoryService productCategoryService)
+        {
+            _productCategoryService = productCategoryService;
+        }
+
+        [HttpGet]
+        public List<ProductCategoryViewModel> GetAll()
+        {
+            return _productCategoryService.GetAll();
+        }
+    }
+}
---
+++ BlazorEF.API/Startup.cs
@@ -0,0 +1,81 @@
+using AutoMapper;
+using BlazorEF.Application.AutoMapper;
+using BlazorEF.Application.Implementation;
+using BlazorEF.Application.Interfaces;
+using BlazorEF.Data.EF;
+using BlazorEF.Data.Entities;
+using BlazorEF.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorEF.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+               options.UseSqlServer(
+                   Configuration.GetConnectionString("DbConnection"),
+                   o => o.MigrationsAssembly("BlazorEF.Data.EF")));
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BlazorEF.API", Version = "v1" });
+            });
+
+            services.AddAutoMapper(typeof(ViewModelToDomainMappingProfile), typeof(DomainToViewModelMappingProfile));
+            IMapper mapper = AutoMapperConfig.RegisterMappings().CreateMapper();
+            services.AddSingleton(mapper);
+
+            services.AddTransient(typeof(IUnitOfWork), typeof(EFUnitOfWork));
+            services.AddTransient(typeof(IRepository<,>), typeof(EFRepository<,>));
+            //Serrvices
+            services.AddTransient<IProductCategoryService, ProductCategoryService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BlazorEF.API v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}

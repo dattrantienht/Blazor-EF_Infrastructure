1682722d0194f94b7b70a0265adb885bcab9c68f
TIENDATPC\tiend <tiendattran.gm@gmail.com>
2021-10-05 22:35:09 +0700

    test api getbyid and post ok

--- BlazorEF/Pages/Counter.razor.cs
+++ BlazorEF/Pages/Counter.razor.cs
@@ -8,11 +8,15 @@ namespace BlazorEF.Pages
 {
     public partial class Counter
     {
-        public List<ProductCategoryViewModel> productCategories;
+        public List<ProductCategoryViewModel> listProductCategories;
+        ProductCategoryViewModel productCategory;
         private string errorString;
+        private string errorString2;
+        private string errorString3;
         public string pcname { get; set; }
         private ProductCategoryViewModel newPC = new ProductCategoryViewModel();
-
+        private int PCid;
+        private string newPcName;
         protected override async Task OnInitializedAsync()
         {
             await Task.Run(getPC);
@@ -23,12 +27,43 @@ namespace BlazorEF.Pages
             var client = _clientFactory.CreateClient("blazor");
             try
             {
-                productCategories = await client.GetFromJsonAsync<List<ProductCategoryViewModel>>("ProductCategory");
+                listProductCategories = await client.GetFromJsonAsync<List<ProductCategoryViewModel>>("ProductCategory");
                 errorString = null;
             }
             catch (Exception ex)
             {
                 errorString = $"Error: {ex.Message}";
+            }
+        }
+
+        public async Task getPCbyID()
+        {
+            var client = _clientFactory.CreateClient("blazor");
+            try
+            {
+                productCategory = await client.GetFromJsonAsync<ProductCategoryViewModel>($"ProductCategory/id={PCid}");
+                errorString2 = null;
+            }
+            catch (Exception ex)
+            {
+                errorString2 = $"Error: {ex.Message}";
+            }
+        }
+
+        public async Task Add()
+        {
+            var client = _clientFactory.CreateClient("blazor");
+            try
+            {
+                newPC.Name = newPcName;
+                await client.PostAsJsonAsync<ProductCategoryViewModel>($"ProductCategory", newPC);
+                await Task.Run(getPC);
+                newPcName = "";
+                errorString3 = null;
+            }
+            catch (Exception ex)
+            {
+                errorString3 = $"Error: {ex.Message}";
             }
         }
     }
